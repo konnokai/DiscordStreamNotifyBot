e99d124b2c6b9eef8f6f30f5213f0cec3d51b6a5
孤之界 <4331068+konnokai@users.noreply.github.com>
2025-08-29 15:27:53 +0800

    feat: 更新 DbContext 的建構子，以方便使用其他的 Data Providers

--- DiscordStreamNotifyBot/DataBase/MainDbService.cs
+++ DiscordStreamNotifyBot/DataBase/MainDbService.cs
@@ -2,16 +2,22 @@
 {
     public class MainDbService
     {
+        private readonly DbContextOptions<MainDbContext> _options;
         private readonly string _connectionString;
 
         public MainDbService(string connectionString)
         {
             _connectionString = connectionString;
+
+            var optionsBuilder = new DbContextOptionsBuilder<MainDbContext>();
+            optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
+            optionsBuilder.UseSnakeCaseNamingConvention();            
+            _options = optionsBuilder.Options;
         }
 
         public MainDbContext GetDbContext()
         {
-            return new MainDbContext(_connectionString);
+            return new MainDbContext(_options);
         }
     }
 }
